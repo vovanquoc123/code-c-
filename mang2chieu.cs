8d202f285afc501471208804f1eb10b87f150ef5
vovanquoc123 <voq0082@gmail.com>
2024-10-04 23:06:13 +0700

    code c
    ode #

---
+++ mang2chieu.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace bien
+{
+    class mang2chieu
+    {
+        public static void Main()
+        {  
+         int a ,b ;
+         Console.WriteLine("chương trình so sánh 2 số chẳn và lẻ ");
+         Console.Write ("nhap so a: ");
+         a = Convert.ToInt32(Console.ReadLine ());
+         b = a /2 ;
+         if (a == 0)
+         {
+          Console.WriteLine("{0} là so chan ", a);
+
+         }
+         else 
+         {
+          Console.WriteLine("{0} là so le ", a);
+         }
+         Console.ReadKey();
+
+    }
+}
+}
